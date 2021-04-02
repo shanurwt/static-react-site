56a44bed1ba0c8df72831fd8602f8baf728fed4c
Ayush <ayushrawat11562@gmail.com>
2021-04-02 22:42:12 +0530

    completed

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,13 +1,15 @@
+import React from 'react'
 import '../../App.css'
 import HeroSection from '../HeroSection'
-import React from 'react'
 import Cards from '../Cards';
+import Footer from '../Footer';
 
 function Home () {
     return (
         <>
             <HeroSection />
             <Cards />
+            <Footer />
         </>
     )
 }
